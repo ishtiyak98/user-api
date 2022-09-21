498b2841f7269924ea04606db95cdc563d5bdbb9
ishtiyak98 <rifatishtiyak@gmail.com>
2022-09-22 02:35:15 +0600

    all users with query

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,13 +1,21 @@
-const fs = require('fs');
+const fs = require("fs");
 const users = require("../model/users.model");
 
-
-module.exports.getAllUser = async (req, res) => {
+//!----------- GET All Users with Query parameter -----------
+module.exports.getAllUser = (req, res) => {
+  const userLimit = req.query?.limit;
   try {
-    res.status(200).json({
-      success: true,
-      data: users,
-    });
+    if (userLimit) {
+      res.status(200).json({
+        success: true,
+        data: users.slice(0, userLimit),
+      });
+    } else {
+      res.status(200).json({
+        success: true,
+        data: users,
+      });
+    }
   } catch (error) {
     res.status(500).send(error.message);
   }
